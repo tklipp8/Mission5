858b1f966c040d0645c30223a8aca4d8a511a65a
tklipp8 <turnerklippel@gmail.com>
2025-02-05 14:15:57 -0700

    Add project files.

---
+++ Mission5/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+// A function that calculates the total cost of turtoring (hours * rate) using jquery while checking to see if the number is negative
+$(document).ready(function () {
+    $("#calculate").click(function () {
+        var hours = $("#hours").val();
+        var rate = 5;
+        var total = hours * rate;
+        if (hours < 0 || rate < 0) {
+            alert("Please enter a positive number");
+        }
+        else {
+            $("#cost").val(total);
+        }
+    });
+});
